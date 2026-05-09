c33a5e85d8259b6d9d8b5b3f0b62ae7042719c7c
Josh Duff <me@JoshDuff.com>
2026-05-09 16:33:28 -0500

    global the autogenerated types too

--- shared/omit.ts
+++ shared/omit.ts
@@ -3,12 +3,12 @@ export const omit = <T extends Record<string, unknown>, K extends keyof T>(
 	remove: readonly K[],
 ): Omit<T, K> => {
 	const result: Partial<T> = {}
-	for (let prop in obj) {
-	  if (!obj.hasOwnProperty || obj.hasOwnProperty(prop)) {
-		if (!remove.includes(prop as unknown as K)) {
-		  result[prop] = obj[prop]
+	for (const prop in obj) {
+		if (!obj.hasOwnProperty || obj.hasOwnProperty(prop)) {
+			if (!remove.includes(prop as unknown as K)) {
+				result[prop] = obj[prop]
+			}
 		}
-	  }
 	}
 	return result as Omit<T, K>
 }
