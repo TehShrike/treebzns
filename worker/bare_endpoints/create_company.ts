acef24b505c6e5e120ba4efdd55f1ed541eb7e0e
Josh Duff <me@JoshDuff.com>
2026-05-19 08:12:05 -0500

    refactor create_company endpoint guts out to its own module, but without the mysql connection juggling

---
+++ worker/bare_endpoints/create_company.ts
@@ -0,0 +1,32 @@
+import { json_response, error_response } from '#worker/lib/response_helpers.ts'
+import type { MysqlHelpersObject } from '#worker/lib/mysql/mysql_helpers_object.ts'
+import { create_company as db_create_company } from '#worker/lib/db/create_company.ts'
+import * as jv from '#shared/json_validator.ts'
+
+const create_company_validator = jv.object({
+	company: jv.object({
+		name: jv.is_string,
+		brand_color: jv.is_string,
+	}),
+	owner_employee: jv.object({
+		name: jv.is_string,
+		email: jv.is_string,
+		phone: jv.is_string,
+		password: jv.is_string,
+	}),
+})
+
+export default async (request: Request, mysql: MysqlHelpersObject): Promise<Response> => {
+	const body = await request.json().catch(() => null)
+	if (!create_company_validator.is_valid(body)) {
+		const messages = create_company_validator.get_messages(body, 'body')
+		return error_response({ message: messages.join(', ') })
+	}
+
+	const company_id = await db_create_company({
+		logo: null,
+		...body.company,
+	}, body.owner_employee, mysql)
+
+	return json_response({ body: { company_id: company_id.toString() }, status: 201 })
+}
--- worker/index.ts
+++ worker/index.ts
@@ -1,40 +1,16 @@
-import { json_response, error_response } from './response_helpers.ts'
+import { json_response, error_response } from '#worker/lib/response_helpers.ts'
 import { create_pool } from '#worker/lib/mysql/connection.ts'
-import make_mysql_helpers_object from '#worker/lib/mysql/mysql_helpers_object.ts'
-import { create_company } from '#worker/lib/create_company.ts'
-import * as jv from '#shared/json_validator.ts'
+import make_mysql_helpers_object, { MysqlHelpersObject } from '#worker/lib/mysql/mysql_helpers_object.ts'
 import type { Pool } from 'mysql2/promise'
+import create_company from './bare_endpoints/create_company.ts'
 
 let pool: Pool | null = null
 
-const create_company_validator = jv.object({
-	company: jv.object({
-		name: jv.is_string,
-		brand_color: jv.is_string,
-	}),
-	owner_employee: jv.object({
-		name: jv.is_string,
-		email: jv.is_string,
-		phone: jv.is_string,
-		password: jv.is_string,
-	}),
-})
-
-const new_company = async (request: Request, pool: Pool): Promise<Response> => {
-	const body = await request.json().catch(() => null)
-	if (!create_company_validator.is_valid(body)) {
-		const messages = create_company_validator.get_messages(body, 'body')
-		return error_response({ message: messages.join(', ') })
-	}
-
+const run_with_connection = async <RESULT>(pool: Pool, fn: (connection: MysqlHelpersObject) => Promise<RESULT>): Promise<RESULT> => {
 	const conn = await pool.getConnection()
 	try {
 		const mysql = make_mysql_helpers_object(conn)
-		const company_id = await create_company({
-			logo: null,
-			...body.company,
-		}, body.owner_employee, mysql)
-		return json_response({ body: { company_id: company_id.toString() }, status: 201 })
+		return await fn(mysql)
 	} finally {
 		conn.release()
 	}
@@ -46,7 +22,7 @@ export default {
 		const { pathname, method } = Object.assign(new URL(request.url), { method: request.method })
 
 		if (method === 'POST' && pathname === '/api/create_company') {
-			return new_company(request, pool)
+			return run_with_connection(pool, async mysql => create_company(request, mysql))
 		} else if (pathname.startsWith('/api/fn/')) {
 			return json_response({ body: { placeholder: true }, status: 200 })
 		}
