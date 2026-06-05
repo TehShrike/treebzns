a0d427a5024e8a1c3b49ea6f2b73183c0d30329e
Josh Duff <me@JoshDuff.com>
2026-06-05 13:14:02 -0500

    Theoretically usable server-side functions available in the client

--- client/lib/server_functions.js
+++ client/lib/server_functions.js
@@ -1,4 +1,11 @@
+import f3tch from '#shared/f3tch.ts'
+
+const call_server_function = (function_name) => async (arg) => f3tch(`/api/fn/${function_name}`, {
+	method: 'POST',
+	body: arg,
+})
+
 export default {
-	create_company: () => {},
-	ping: () => {}
+	create_company: call_server_function('create_company'),
+	ping: call_server_function('ping'),
 }
